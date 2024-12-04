03817f9b78ab0e12df2d4d8f49ebc7c3c4f25368
khsckl3690 <129178582+khsckl3690@users.noreply.github.com>
2024-12-04 20:19:26 +0900

    worker_code inserted
    .

--- db_prac2/src/main/java/com/example/jdbc/Main.java
+++ db_prac2/src/main/java/com/example/jdbc/Main.java
@@ -91,12 +91,14 @@ public class Main {
                         }
                         break;
                     case 2:
+                        /* 매출 */
                         System.out.println("[1] sell\n" /* 판매(결제) */ +
-                                "[2] \n" /* 매출 조회 */ +
+                                "[2] \n" /* 판매 상품 조회 */ +
                                 "[3] refund\n" /* 매입/배송 조회[업데이트] */ +
-                                "[4] exit\n" +
+                                "[4] return to the menu\n" +
                                 "Enter your command: ");
                         user_input = scanner.nextInt();
+                        System.out.print("\n\n");
 
                         switch (user_input){
                             case 1:
@@ -125,12 +127,7 @@ public class Main {
                                 }
                                 break;
                             case 2:
-                                System.out.println("[1] \n" /* 재고 조회 */ +
-                                        "[2] sale\n" /* 매출 조회[업데이트] */ +
-                                        "[3] purchase/delivery\n" /* 매입/배송 조회[업데이트] */ +
-                                        "[4] exit\n" +
-                                        "Enter your command: ");
-                                user_input = scanner.nextInt();
+                                /* 판매 상품 조회 */
                                 break;
                             case 3:
                                 if (position_id == 3) { // if the worker is a normal employee
@@ -145,30 +142,83 @@ public class Main {
                                 }
                                 break;
                             case 4:
-
-                                return;
+                                break;
                         }
                         break;
                     case 3:
+                        /* 매입 */
                         if (position_id == 3) { // if the worker is a normal employee
-                            System.out.println("You don't have privileges");
+                            System.out.println("You don't have the privileges");
                         }
                         else{
-                            System.out.println("[1] check inventory\n" /* 재고 조회 */ +
-                                    "[2] sale\n" /* 매출 조회[업데이트] */ +
-                                    "[3] purchase/delivery\n" /* 매입/배송 조회[업데이트] */ +
+                            System.out.println("[1] purchase\n" /* 매입 */ +
+                                    "[2] check whether the delivery is completed\n" /* 매입에 따른 배송 내역 확인 */ +
+                                    "[3] recall\n" /*  */ +
                                     "[4] exit\n" +
                                     "Enter your command: ");
+                            user_input = scanner.nextInt();
+                            System.out.print("\n\n");
+
+                            switch (user_input){
+                                case 1:
+                                    /*  */
+                                    bufferSQL = "SELECT p.prod_id, p.prod_name, b.brand_name, i.inventory\n" +
+                                            "FROM product p\n" +
+                                            "JOIN prod_brand b ON p.brand_id = b.brand_id\n" +
+                                            "JOIN inventory i ON p.prod_id = i.prod_id\n" +
+                                            "WHERE i.store_id = " + store_id + ";";
+
+                                    /* print the inventory */
+                                    try (Statement statement = connection.createStatement();
+                                         ResultSet resultSet = statement.executeQuery(bufferSQL)) {
+                                        System.out.printf("%-10s %-40s %-20s %-10s%n", "prod_id", "prod_name", "brand_name", "inventory");
+                                        System.out.println("--------------------------------------------------------------");
+
+                                        while (resultSet.next()) {
+                                            int prodId = resultSet.getInt("prod_id");
+                                            String prodName = resultSet.getString("prod_name");
+                                            String brandName = resultSet.getString("brand_name");
+                                            int inventory = resultSet.getInt("inventory");
+
+                                            System.out.printf("%-10d %-40s %-20s %-10d%n", prodId, prodName, brandName, inventory);
+                                        }
+                                        System.out.print("\n\n");
+                                    }
+                                    break;
+                                case 2:
+                                    /* 판매 상품 조회 */
+                                    break;
+                                case 3:
+                                    if (position_id == 3) { // if the worker is a normal employee
+                                        System.out.println("You don't have privileges");
+                                    }
+                                    else{
+                                        System.out.println("[1] check inventory\n" /* 재고 조회 */ +
+                                                "[2] sale\n" /* 매출 조회[업데이트] */ +
+                                                "[3] purchase/delivery\n" /* 매입/배송 조회[업데이트] */ +
+                                                "[4] exit\n" +
+                                                "Enter your command: ");
+                                    }
+                                    break;
+                                case 4:
+                                    break;
+                            }
                         }
                         break;
                     case 4:
-                        bufferSQL = "SELECT money\n" +
-                                "FROM money\n" +
-                                "WHERE store_id = " + store_id + ";";
+                        if (position_id == 1) {
+                            /* 금고 조회 */
+                            bufferSQL = "SELECT money\n" +
+                                    "FROM money\n" +
+                                    "WHERE store_id = " + store_id + ";";
 
-                        try (Statement statement = connection.createStatement();
-                             ResultSet resultSet = statement.executeQuery(bufferSQL)) {
-                             System.out.println("available money: " + resultSet.getInt("money"));
+                            try (Statement statement = connection.createStatement();
+                                 ResultSet resultSet = statement.executeQuery(bufferSQL)) {
+                                System.out.println("available money: " + resultSet.getInt("money"));
+                            }
+                        }
+                        else{
+                            System.out.println("You don't have the privileges");
                         }
                         break;
                     case 5:
@@ -178,7 +228,8 @@ public class Main {
             }
 
 
-            // deletable
+            // should be deleted
+            /*
             try (Statement statement = connection.createStatement();
                  ResultSet resultSet = statement.executeQuery(bufferSQL)) {
                 while (resultSet.next()) {
@@ -186,6 +237,7 @@ public class Main {
                             ", i_ID: " + resultSet.getString("i_id"));
                 }
             }
+            */
 
         } catch (Exception e) {
             e.printStackTrace();
